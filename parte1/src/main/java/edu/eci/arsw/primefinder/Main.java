ad35c6a9450637b5ae45c20450fa1b6ad11d8169
J-382 <jrperezdeleon@gmail.com>
2021-08-20 17:37:55 -0500

    Punto 1.3

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,9 +1,52 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ScheduledExecutorService;
+
 public class Main {
 
-	public static void main(String[] args) {
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		pft.start();
+	public static void main(String[] args) throws InterruptedException{
+		int nthreads  = 20;
+		LinkedList<PrimeFinderThread> threads = new LinkedList<PrimeFinderThread>();
+		pauseThreadsAfterDelay(threads,2000); /* Pauses all threads after 5 seconds*/
+		int range = 30000000/nthreads;
+		for(int i=0; i < 30000000; i+=range){
+			int lowerBound = i == 0 ? 2 : i, upperBound = i+range > 30000000 ? 30000000 : i+range;
+			PrimeFinderThread pft = new PrimeFinderThread(lowerBound, upperBound);
+			threads.add(pft);
+			pft.start();
+		}
+	}
+
+	/**
+	 * Pauses all threads after a given delay and wait for user confirmation to resume execution 
+	 * @param threads 
+	 * @param delay
+	 */
+	private static void pauseThreadsAfterDelay(LinkedList<PrimeFinderThread> threads, int delay){
+		
+		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+		executorService.schedule(new Runnable(){
+			@Override
+			public void run() {
+				PrimeFinderThread.stopThreads();
+				System.out.println("Se han encontrado "+PrimeFinderThread.getPrimes().size()+
+				" primos hasta ahora.\nPresione Enter para continuar la ejecucion...");
+
+				Scanner sc = new Scanner(System.in);
+				sc.nextLine();
+				sc.close();
+
+				PrimeFinderThread.resumeThreads();
+				for(Thread thread: threads) try {thread.join();} catch (Exception e) {}
+
+				System.out.println("Se encontraron "+PrimeFinderThread.getPrimes().size()+" primos.");
+				System.out.println("Done.");
+				executorService.shutdown();
+			}
+		}, delay, TimeUnit.MILLISECONDS);
 	}
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -1,14 +1,15 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PrimeFinderThread extends Thread{
 
-	
 	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
+	private static AtomicBoolean stopped = new AtomicBoolean(false);
+	private static List<Integer> primes = Collections.synchronizedList(new LinkedList<Integer>());
 	
 	public PrimeFinderThread(int a, int b) {
 		super();
@@ -16,27 +17,62 @@ public class PrimeFinderThread extends Thread{
 		this.b = b;
 	}
 
+	/**
+	 * Find all primes in a range
+	 */
 	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
+		for (int i=a;i<=b;i++){	
+			synchronized (stopped){
+				while (stopped.get()){
+					try {stopped.wait();}
+					catch (Exception e) {}
+				}
 			}
+			if (isPrime(i)) primes.add(i);			
 		}
-		
-		
 	}
 	
+	/**
+	 * Check if the given number it's a prime
+	 * @param n Number to be checked
+	 * @return True if the number is a prime, false otherwise
+	 */
 	boolean isPrime(int n) {
-		if (n%2==0) return false;
-		for(int i=3;i*i<=n;i+=2) {
-			if(n%i==0)
-				return false;
-		}
-		return true;
+	    if (n%2==0 && n!=2) return false;
+	    for(int i=3;i*i<=n;i+=2) {
+	        if(n%i==0) return false;
+	    }
+	    return true;
 	}
 
-	public List<Integer> getPrimes() {
+	public static List<Integer> getPrimes() {
 		return primes;
+	}
+	
+	/**
+	 * Thread-safe function to add a new prime 
+	 * @param i the new prime to be added
+	 */
+	synchronized void addPrime(int i){
+		primes.add(i);
+	}
+
+	/**
+	 * Changes stopped flag state to True and notify this to all threads waiting 
+	 */
+	public static void stopThreads(){
+		synchronized (stopped){
+			stopped.set(true);
+		}
+	}
+
+	/**
+	 * Changes stopped flag state to false and notify this to all threads waiting 
+	 */
+	public static  void resumeThreads() {
+		synchronized (stopped) {
+		  stopped.set(false);
+		  stopped.notifyAll();
+		}
 	}
 }
